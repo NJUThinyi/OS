98969dc868d55cf3e3f684ce7a72b6563dbed314
NJUThinyi <171250592@smail.nju.edu.cn>
2019-12-25 15:55:16 +0800

    struct

--- lab4/include/global.h
+++ lab4/include/global.h
@@ -31,9 +31,9 @@ extern	irq_handler	irq_table[];
 
 EXTERN int reader_count;
 EXTERN int writer_count;
-EXTERN SEMAPHORE rmutex; 
-EXTERN SEMAPHORE rmutex2; 
-EXTERN SEMAPHORE wmutex;
-EXTERN SEMAPHORE S;
+EXTERN struct semaphore rmutex; 
+EXTERN struct semaphore rmutex2; 
+EXTERN struct semaphore wmutex;
+EXTERN struct semaphore S;
 EXTERN int rw_prio; //0：读者优先（默认），1：写者优先
 EXTERN int r_w_now;
--- lab4/kernel/global.c
+++ lab4/kernel/global.c
@@ -33,10 +33,10 @@ PUBLIC	system_call		sys_call_table[NR_SYS_CALL] = {sys_get_ticks, sys_process_sl
 //增加的关于读者写者的全局变量
 PUBLIC int reader_count;
 PUBLIC int writer_count;
-PUBLIC SEMAPHORE rmutex; 
-PUBLIC SEMAPHORE rmutex2; 
-PUBLIC SEMAPHORE wmutex;
-PUBLIC SEMAPHORE S;
+PUBLIC struct semaphore rmutex; 
+PUBLIC struct semaphore rmutex2; 
+PUBLIC struct semaphore wmutex;
+PUBLIC struct semaphore S;
 PUBLIC int BOOKS;
 PUBLIC int rw_prio; //0：读者优先（默认），1：写者优先
 PUBLIC int r_w_now; /* 0: 当前为读进程， 1：当前为写进程*/
