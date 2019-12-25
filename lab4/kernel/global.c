dc4d0d4ea8bd5e20b02435d1866b747f85a4948a
NJUThinyi <171250592@smail.nju.edu.cn>
2019-12-25 20:35:03 +0800

    系统调用

--- lab4/kernel/global.c
+++ lab4/kernel/global.c
@@ -28,7 +28,11 @@ PUBLIC	TASK	task_table[NR_TASKS] = {{TestA, STACK_SIZE_TESTA, "TestA"},
 
 PUBLIC	irq_handler		irq_table[NR_IRQ];
 
-PUBLIC	system_call		sys_call_table[NR_SYS_CALL] = {sys_get_ticks, sys_process_sleep, sys_my_disp_str, sys_P, sys_V};
+PUBLIC	system_call		sys_call_table[NR_SYS_CALL] =   {sys_get_ticks, 
+                                                         sys_process_sleep, 
+                                                         sys_my_disp_str,
+                                                         sys_P, 
+                                                         sys_V};
 
 //增加的关于读者写者的全局变量
 PUBLIC int reader_count;
